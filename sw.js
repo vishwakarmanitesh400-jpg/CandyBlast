9f7a3769a398c9456aeb502f798940287023b338
vishwakarmanitesh400-jpg <vishwakarmanitesh400@gmail.com>
2026-08-23 15:35:59 +0530

    Update service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-const CACHE='candy-v1';self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME = 'candy-v10-NEW-MAP';
+const urlsToCache = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});
