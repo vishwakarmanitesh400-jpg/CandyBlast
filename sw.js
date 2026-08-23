393d33bd167aadb229d2edfa5bb98cba8b05fb41
vishwakarmanitesh400-jpg <vishwakarmanitesh400@gmail.com>
2026-08-23 16:42:46 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME='candy-professional-v1';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>{
-  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
