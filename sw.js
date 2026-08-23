9460ff6855b7e4df9042b2f5382ca5998f7c6135
vishwakarmanitesh400-jpg <vishwakarmanitesh400@gmail.com>
2026-08-23 15:55:04 +0530

    Refactor service worker caching logic
    Updated service worker to use new cache name and simplified event listeners.

--- sw.js
+++ sw.js
@@ -1,18 +1,7 @@
-const CACHE_NAME = 'candy-v10-NEW-MAP';
-const urlsToCache = ['./', './index.html'];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))
-  );
+const CACHE_NAME='candy-professional-v1';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
-});
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request)));
